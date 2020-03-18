d58093ed66de76aa5345ae1d0b72957aaf1eccd3
Sanket Patel <sanketpatel0512@gmail.com>
2020-03-18 18:52:02 -0400

    Copy all files from Project_Part1

---
+++ main.py
@@ -0,0 +1,45 @@
+from sodapy import Socrata
+import json
+import argparse
+import os
+
+#Define Main Function For NYC Parking Violation Data Collection & Output
+def main(page_size,num_pages,output):
+	client = Socrata("data.cityofnewyork.us",dict(os.environ)["APP_KEY"]) #APP_KEY is APP_TOKEN from Socrata
+	off_set = 0
+
+	# If statement to define number of calls if input not provided by user
+	if num_pages == False:
+		lines = client.get("nc67-uf89",select='COUNT(*)')
+		int_count = int(lines[0]['COUNT'])
+		num_pages = (int_count//page_size)+1
+
+	# Open Output file provided by user		
+	if output != False:
+		outfile = open(output, 'w')
+
+	#For loop to Call Data from NYC Open Data
+	for i in range(num_pages):
+		d = client.get("nc67-uf89",limit=page_size,offset = off_set)
+		# Output data based on user's choice
+		# Print Data In Stdout
+		if output == False:
+			for i in d:
+				print(i)
+		# Save data to output file
+		else:
+			for i in d:
+				json.dump(i, outfile)
+				outfile.write('\n')
+		off_set += page_size
+
+# Define and parse Commmandline arguments from user
+parser = argparse.ArgumentParser(description = 'Inputs for Parking Violation Data Collection')
+parser.add_argument('--page_size',type = int, required =True, help = 'Required Argument: Input number of data records to pull in one call')
+parser.add_argument('--num_pages', type = int, default = False,help = 'Optional Argument: Input number of calls to perform on the dataset')
+parser.add_argument('--output', type = str, default = False,help = 'Optional Argument: Provide *.json file name to save data in file')
+		
+# Pass the argument inputs to the main function
+if __name__ == '__main__':
+	args = parser.parse_args()
+	main(**vars(args))
